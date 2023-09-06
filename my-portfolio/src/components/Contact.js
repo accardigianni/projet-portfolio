ac83c2432f67f879e84dcfff5863c6419898c024
accardigianni <gianni.accardi@epitech.eu>
2023-09-06 14:56:28 +0200

    feat : form and googlemap update

---
+++ my-portfolio/src/components/Contact.js
@@ -0,0 +1,104 @@
+import React from "react";
+
+export default function Contact() {
+    return (
+        <section id="contact" className="relative">
+            <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+                <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                    <iframe
+                        width="100%"
+                        height="100%"
+                        title="map"
+                        className="absolute inset-0" // display map in the entire div
+                        style={{ filter: "opacity(0.7)" }}
+                        src="https://www.google.com/maps/embed/v1/place?q=Nantes,+France&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+                    />
+                    <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+                        <div className="lg:w-1/2 px-6">
+                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                                ACCARDI Gianni
+                            </h2>
+                            <p className="mt-1">
+                                74, boulevard de la prairie au duc <br />
+                                44200 Nantes
+                            </p>
+                        </div>
+                        <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                            <h2 className="Email title-font font-semibold text-white tracking-widest text-xs">
+                                EMAIL
+                            </h2>
+                            <a className="text-indigo-400 Email">
+                                gianniac44@gmail.com
+                            </a>
+                            <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                                PHONE
+                            </h2>
+                            <p className="phone">06 24 67 64 63</p>
+                        </div>
+                    </div>
+                </div>
+                <form
+                    netlify //enable you to accept dynamically-driven user data on a static site
+                    name="contact"
+                    className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                        Contactez moi
+                    </h2>
+                    <p className="leading-relaxed mb-5">
+                        Vous êtes à la recherche d'un étudiant alternant motivé et prêt à ce mettre au service de votre structure, remplissez les champs suivants :
+                    </p>
+                    <div className="relative mb-4">
+                        <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+                            Votre Nom et prénom
+                        </label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        />
+                    </div>
+                    <div className="relative mb-4">
+                        <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+                            Votre contact ( Email ou numéro de téléphone)
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        />
+                    </div>
+                    <div className="relative mb-4">
+                        <label htmlFor="company" className="leading-7 text-sm text-gray-400">
+                            Nom de votre structure
+                        </label>
+                        <input
+                            type="text"
+                            id="company"
+                            name="company"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        />
+                    </div>
+                    <div className="relative mb-4">
+                        <label
+                            htmlFor="message"
+                            className="leading-7 text-sm text-gray-400">
+                            Que recherchez vous ?
+                        </label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                        Envoyer
+                    </button>
+                </form>
+            </div>
+        </section>
+    );
+}
