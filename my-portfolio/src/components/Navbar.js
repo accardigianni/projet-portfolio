8b65c64e9b006fa6ba863a9f5a7b5d34066031c7
accardigianni <gianni.accardi@epitech.eu>
2023-09-11 15:56:49 +0200

    button in navbar change color

--- my-portfolio/src/components/Navbar.js
+++ my-portfolio/src/components/Navbar.js
@@ -6,23 +6,23 @@ export default function Navbar() {
   return (
     <header className="bg-eggplant md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <div className="title-font font-medium text-snow hover:text-aquamarine mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
+        <div className="title-font font-medium hover:text-aquamarine mb-4 md:mb-0">
+          <a href="#about" className="ml-3 text-xl  bg-aquamarine text-s border-1 py-1 px-3  hover:bg-gray hover:text-snow rounded">
             Présentation
           </a>
         </div>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-snow	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 text-snow hover:text-aquamarine font-medium">
+          <a href="#projects" className="mr-5 inline-flex items-center bg-aquamarine border-1 py-1 px-3  hover:bg-gray hover:text-snow rounded text-base mt-4 md:mt-0">
             Projets
           </a>
-          <a href="#techno" className="mr-5 text-snow hover:text-aquamarine font-medium">
+          <a href="#techno" className="mr-5 inline-flex items-center bg-aquamarine border-1 py-1 px-3 hover:bg-gray hover:text-snow rounded text-base mt-4 md:mt-0">
             Technologies
           </a>
-          <a href="#skills" className="mr-5 text-snow hover:text-aquamarine font-medium">
+          <a href="#skills" className="mr-5 inline-flex items-center bg-aquamarine border-1 py-1 px-3  hover:bg-gray hover:text-snow rounded text-base mt-4 md:mt-0">
             Soft Skills
           </a>
         </nav>
-        <Download/>
+        <Download />
       </div>
     </header>
   );
