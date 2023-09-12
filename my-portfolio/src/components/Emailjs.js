bfbb92cfa9fd32ccc27d69e617ca82577be75266
accardigianni <gianni.accardi@epitech.eu>
2023-09-12 15:13:41 +0200

    cache public key

--- my-portfolio/src/components/Emailjs.js
+++ my-portfolio/src/components/Emailjs.js
@@ -3,5 +3,5 @@ const scriptElement = document.createElement('script');
 scriptElement.src = 'https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js';
 document.body.insertAdjacentElement('beforeend', scriptElement);
 
-const inlineScript = '(function() { emailjs.init("YOUR_PUBLIC_KEY"); })();';
+const inlineScript = '(function() { emailjs.init("${process.env.Public_key}"); })();';
 document.body.insertAdjacentHTML('beforeend', `<script type="text/javascript">${inlineScript}</script>`);
