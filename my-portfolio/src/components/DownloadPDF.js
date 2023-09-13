fb2fb13fd0c533fa1c483da7ef05330d20781855
accardigianni <gianni.accardi@epitech.eu>
2023-09-13 22:22:41 +0200

    target = blank

--- my-portfolio/src/components/DownloadPDF.js
+++ my-portfolio/src/components/DownloadPDF.js
@@ -7,6 +7,7 @@ export default function Download() {
     return (
         <a
             href={CV}
+            target="#blank"
             className="inline-flex items-center bg-lightblue border-0 py-1 px-3 focus:outline-none hover:bg-strongblue hover:text-lightblue rounded text-base mt-4 md:mt-0">
             Télécharger mon CV
             <FolderDownloadIcon className="w-5 h-5 ml-2" />
