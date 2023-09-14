a02f7da8911aad5763075c1216504b64d1fa04fc
accardigianni <gianni.accardi@epitech.eu>
2023-09-14 11:10:40 +0200

    target blank to link project

--- src/components/Showproject.js
+++ src/components/Showproject.js
@@ -2,34 +2,35 @@ import React from "react";
 import { projects } from "../Data";
 
 
-export default function ShowProject(){
-    return(
-    <section id="show project">
-        <div className="flex flex-wrap -m-4">
-                    {projects.map((project) => (
-                        <a
-                            href={project.link}
-                            key={project.image}
-                            className="sm:w-1/2 w-full h-full p-4">
-                            <div className="flex relative ">
-                                <img
-                                    alt="project description"
-                                    className="absolute w-full h-full  object-cover object-center"
-                                    src={project.image}
-                                />
-                                <div className="px-8 py-10 relative z-10 w-full border-4 border-aquamarine bg-gray opacity-0 hover:opacity-100">
-                                    <h2 className="tracking-widest text-sm title-font font-medium text-aquamarine mb-1">
-                                        {project.subtitle}
-                                    </h2>
-                                    <h1 className="title-font text-lg font-medium text-aquamarine mb-3">
-                                        {project.title}
-                                    </h1>
-                                    <p className="leading-relaxed text-aquamarine">{project.description}</p>
-                                </div>
+export default function ShowProject() {
+    return (
+        <section id="show project">
+            <div className="flex flex-wrap -m-4">
+                {projects.map((project) => (
+                    <a
+                        href={project.link}
+                        key={project.image}
+                        target="#blank"
+                        className="sm:w-1/2 w-full h-full p-4">
+                        <div className="flex relative ">
+                            <img
+                                alt="project description"
+                                className="absolute w-full h-full  object-cover object-center"
+                                src={project.image}
+                            />
+                            <div className="px-8 py-10 relative z-10 w-full border-4 border-aquamarine bg-gray opacity-0 hover:opacity-100">
+                                <h2 className="tracking-widest text-sm title-font font-medium text-aquamarine mb-1">
+                                    {project.subtitle}
+                                </h2>
+                                <h1 className="title-font text-lg font-medium text-aquamarine mb-3">
+                                    {project.title}
+                                </h1>
+                                <p className="leading-relaxed text-aquamarine">{project.description}</p>
                             </div>
-                        </a>
-                    ))}
-                </div>
-    </section>
+                        </div>
+                    </a>
+                ))}
+            </div>
+        </section>
     )
 }
